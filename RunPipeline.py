1b2da77ebc3bdad2bf476702de801387a84f26f9
Joe Michail <josephmichail@gmail.com>
2025-10-12 17:23:36 -0400

    Update RunPipeline.py
    Fixed relative path in RunPipeline.py file for consistency with README

--- RunPipeline.py
+++ RunPipeline.py
@@ -1,7 +1,7 @@
 from MRS_LC_Calibration import MRS_LightCurve_Calibration
 
 # Instantiate light curve processing object
-processor = MRS_LightCurve_Calibration("./jw04572003001*")
+processor = MRS_LightCurve_Calibration("./JWST/jw*")
 
 # Go through each folder starting with above code and calibrate through Level 3
 processor.process()
